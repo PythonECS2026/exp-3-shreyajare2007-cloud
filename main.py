74397b3683c54cd673aa41132cda109f0bebc976
shreyajare2007-cloud <shreyajare2007@gmail.com>
2026-02-11 11:14:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 # AIM: Write a Python program to calculate the gross salary of an employee.
 # Coder:
 # Date:
+basesalary = float(input("Enter the employee's base salary:"))
+print("Basic Salary:",basesalary)
+ 
+DA = 0.7*basesalary
+print("DA(70%):",DA) 
+TA = 0.3*basesalary
+print("TA(30%):",TA)
+HRA = 0.10*basesalary
+print("HRA(10%):",HRA)
 
+Grosssalary = DA+TA+HRA
+print("grosssalary:",Grosssalary)
 # Write your code here
